71b3c2401e1cbad9dc86e9479ca9d0bcd6fc3a07
Miguel Matos <miguelamatos@ua.pt>
2023-02-19 01:21:47 +0000

    swap porque ocd

--- lab01/stack-contract/src/test/java/mankings/tqs/StackTQSTests.java
+++ lab01/stack-contract/src/test/java/mankings/tqs/StackTQSTests.java
@@ -8,8 +8,8 @@ import org.junit.jupiter.api.DisplayName;
 import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class StackTQSTests {
