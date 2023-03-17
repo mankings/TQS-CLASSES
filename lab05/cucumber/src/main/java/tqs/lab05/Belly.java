7c9e8bc503493b5d35ac9614169d31dfeaab60c2
Miguel Matos <miguelamatos@ua.pt>
2023-03-17 12:13:41 +0000

    cucumba

---
+++ lab05/cucumber/src/main/java/tqs/lab05/Belly.java
@@ -0,0 +1,16 @@
+package tqs.lab05;
+
+public class Belly {
+    public void eat(int cukes) {
+
+    }
+
+    public void wait(int hours) {
+
+    }
+
+    public void growl() {
+        
+    }
+
+}
---
+++ lab05/cucumber/src/test/java/tqs/lab05/StepDefinitions.java
@@ -0,0 +1,24 @@
+package tqs.lab05;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class StepDefinitions {
+    @Given("I have {int} cukes in my belly")
+    public void I_have_cukes_in_my_belly(int cukes) {
+        Belly belly = new Belly();
+        belly.eat(cukes);
+    }
+
+    @When("I wait {int} hour")
+    public void i_wait_hour(int time) {
+        Belly belly = new Belly();
+        belly.wait(time);
+    }
+
+    @When("my belly should growl")
+    public void my_belly_should_growl() {
+        Belly belly = new Belly();
+        belly.growl();
+    }
+}
